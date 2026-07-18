610641d2354749a140ba9941aebbd57d642d45f8
somehow <snsdkf6@gmail.com>
2026-07-18 17:33:39 +0900

    [fix] enableCors 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as bodyParser from "body-parser";
+import { Logger } from "@nestjs/common/services/logger.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "http://localhost:3000",
+    allowedHeaders: ["content-type"],
+    // origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
+
   app.use(bodyParser.json({ limit: "50mb" }));
   app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-  app.enableCors();
+
   await app.listen(process.env.PORT || 3040);
+  Logger.log(`Server is running on ${(await app.getUrl()).toString()}`);
 }
+
 bootstrap();
