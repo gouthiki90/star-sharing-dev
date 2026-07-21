04a576a772d2525a967dcd266ff8bcd923884057
somehow <snsdkf6@gmail.com>
2026-07-21 14:49:00 +0900

    [chore] jwt 가드 추가

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -1,7 +1,15 @@
-import { Body, Controller, Post, UseFilters } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseFilters,
+  UseGuards,
+} from "@nestjs/common";
 import { LocationDto } from "./dto/location.dto";
 import { LocationService } from "./location.service";
 import { ErrorHandler } from "../common/error-handler";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 
 @UseFilters(ErrorHandler)
 @Controller("location")
@@ -10,5 +18,11 @@ export class LocationController {
   @Post()
   getLocation(@Body() locationDto: LocationDto) {
     return this.locationService.getLocation(locationDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  forGuardTest() {
+    return "sent req";
   }
 }
